29da2dcda13aa56ba20118d6eef9984a6c7eba4f
TauDuque <hectauduque@gmail.com>
2021-09-22 14:48:01 -0300

    dashboard styles

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo, useEffect} from 'react'; 
-import { SelectStyle } from './style';
+import { SelectStyle, Speedometer } from './style';
+import {format, Locale} from "date-fns"
 import {
     Button,
     Loading,
@@ -19,8 +20,8 @@ interface SelectProps {
 }
 
 export default function Dashboard({className}: SelectProps) {
-
-   
+    
+    
  
 const input = [<Calendar size="32px" className="field"/>, "Ano" ]
 
@@ -31,11 +32,27 @@ const input = [<Calendar size="32px" className="field"/>, "Ano" ]
     <Text h2>Dashboard</Text>
     <SelectStyle>
   <select title="Ano" name="Ano" id="ano">
-      <option value="" disabled selected>Ano</option>
-     {ano.map((item, i) => 
-         <option key={i} value={item}>{item}</option>
-     )}
+    <option value="" disabled selected>Ano</option>
+    <option value="2010" >2010</option>
   </select>
+<Speedometer>
+  <div id="main-div">
+  {/* Scorer 1 Starts */}
+  <div className="layout-align">
+    <div id="score-meter-1" className="layout-align">
+    <div id="scorer-1-inner-div">
+      <div id="scorer-1-inner-div-5">
+        <div className="scorer-1-tick">
+        </div>
+      </div>
+    </div>
+    <div id="scorer-1-inner-div-2"></div>
+    <div id="scorer-1-inner-div-3"></div>
+    <div id="scorer-1-inner-div-4"></div>
+  </div>
+  </div>
+  </div>
+  </Speedometer>
     </SelectStyle>
     </>
 }
