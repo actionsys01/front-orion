d870766dfbea03bacf01636563306d106a9cd048
TauDuque <hectauduque@gmail.com>
2021-09-30 15:26:40 -0300

    context

--- src/contexts/security.tsx
+++ src/contexts/security.tsx
@@ -1,13 +1,31 @@
-import React, { useEffect, useContext, useReducer } from "react";
+import React, { useEffect, useContext, useReducer, useState } from "react";
 import { useSession } from "next-auth/client";
 
-interface ContextProps  {};
+interface ContextProps  {
+
+};
 
 const SecurityContext = React.createContext({} as ContextProps);
 
 const SecurityProvider: React.FC = ({ children }: any) => {
-  const session = useSession()
-  console.log('contextSession:', session);
+  const [session] = useSession();
+  const permissions = session?.usuario.perfil.permissoes;
+  const [userPermission, setUserPermission] = useState<boolean>(true)
+  const [profilePermission, setProfilePermission] = useState<boolean>(false)
+  const [nfePermission, setNfePermission] = useState<boolean>(false)
+  const [ctePermission, setCtePermission] = useState<boolean>(false)
+  const [nfsePermission, setNfsePermission] = useState<boolean>(false)
+  const [entrancePermission, setEntrancePermission] = useState<boolean>(false)
+  console.log("permissions:", permissions);
+
+
+const getPermission = () => {
+
+}
+
+useEffect(() => {
+  getPermission()
+}, [session])
   
 
   return <SecurityContext.Provider value="value">{children}</SecurityContext.Provider>;
