ec0dca4e822521cb299269d1e1dc7354d11caf34
TauDuque <hectauduque@gmail.com>
2021-10-18 09:03:28 -0300

    styled screen for company register section

--- src/pages/cadastrar-empresa/index.tsx
+++ src/pages/cadastrar-empresa/index.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Head from "next/head";
 import { useRouter } from "next/router";
 import BotaoVoltar from "@components/BotaoVoltar";
 import {CompanyRegister} from "./style";
-import { BottomConfirmBtn } from 'src/styles/buttons';
-import { Select } from 'src/styles/select'; 
+import { BottomConfirmBtn } from 'src/styles/buttons'; 
+import * as companies from "@services/empresas"
+import { useCallback } from 'hoist-non-react-statics/node_modules/@types/react';
+import { useToasts, Modal, useModal } from "@geist-ui/react";
 
 export default function CadastrarEmpresa() {
     const router = useRouter();
+    const [company, setCompany] = useState<string>("");
+    const [cnpj, setCnpj] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [phone, setPhone] = useState<string>("");
+    const [account, setAccount] = useState<number[]>([]);
+    const [, setToast] = useToasts()
+    
+    // const gatherData = useCallback(() => {
+
+    // }, [])
+
+    async function createCompany() {
+        try {
+            
+            setToast({
+                text: "Empresa cadastrada com sucesso.",
+                type: "success"
+            })
+        } catch (error) {
+            setToast({
+                text: "Houve um problema, por favor tente novamente.",
+                type: "warning"
+            })
+        }
+    }
+    
     return (
         <>
             <Head>
@@ -20,22 +48,22 @@ export default function CadastrarEmpresa() {
                     <div className="container">
                     <div className="label"><h6>Empresa</h6></div>
                         <div className="input-style">
-                            <input type="text" /* onChange={(e) => setName(e.target.value)} *//>
+                            <input type="text" onChange={(e) => setCompany(e.target.value)}/>
                         </div>
                         <div className="label"><h6>CNPJ</h6></div>
                         <div className="input-style">
-                            <input type="text" /* onChange={(e) => setName(e.target.value)} *//>
+                            <input type="text" onChange={(e) => setCnpj(e.target.value)}/>
                         </div>
                         <div className="label"><h6>E-mail</h6></div>
                         <div className="input-style">
-                            <input type="text" /* onChange={(e) => setName(e.target.value)} *//>
+                            <input type="text" onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div className="label"><h6>Telefone</h6></div>
                         <div className="input-style">
-                            <input type="text" /* onChange={(e) => setName(e.target.value)} *//>
+                            <input type="text" onChange={(e) => setPhone(e.target.value)}/>
                         </div>
                         <div className="label"><h6>Plano</h6></div>
-                    <select name="" id=""></select>
+                        <select name="" id=""></select>
                     </div>
                 </div>
             </CompanyRegister>
--- src/pages/empresas/index.tsx
+++ src/pages/empresas/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import BotaoVoltar from "@components/BotaoVoltar";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { MoreHorizontal,Plus } from "@geist-ui/react-icons";
 import { CompaniesGrid } from './style';
-import {AddBtn} from "../../styles/buttons"
+import {AddBtn} from "../../styles/buttons";
+import * as companies from "@services/empresas"
+import Popover from '@components/Popover';
 
 export default function Empresas() {
     const router = useRouter();
+    
+
+//    async function getData() {
+//         const response = await companies.getCompanies()
+//         const data = response.data
+//         // console.log("compa:",data)    
+//         return data
+//     }
+
+    // useEffect(() => {
+    //    getData().then(response => setTest(response))
+      
+    // }, [])ss
+    
+
     return (
         <>
         <Head>
@@ -36,7 +53,7 @@ export default function Empresas() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td><MoreHorizontal /></td>
+                        <td><Popover /></td>
                         <td>AÇUCAR UNIÃO SA</td>
                         <td>85 256 285 0001</td>
                         <td>Master</td>
