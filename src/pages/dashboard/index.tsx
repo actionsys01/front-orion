5dd8beefaa4b40a26e4d60fd5a87cb81a5ad2c8e
TauDuque <hectauduque@gmail.com>
2021-11-16 08:16:28 -0300

    dashboard features

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,87 +1,144 @@
-import React, { useState, useMemo, useEffect} from 'react'; 
-import { SelectStyle, Speedometer } from './style';
+import React, { useState, useMemo, useEffect, useCallback} from 'react'; 
+import { SelectStyle, Speedometer, InfoContainer } from './style';
 import {format, Locale} from "date-fns"
 import { useSession } from "next-auth/client";
 import Head from "next/head";
 import {useSecurityContext} from "@contexts/security"
+import GaugeChart from "react-gauge-chart"
+import { BsChevronCompactUp } from "react-icons/bs";
+import * as planos from "@services/planos"
+import * as empresas from "@services/empresas"
 
-interface SelectProps {
-    className: string
-}
 
-interface IUsuario  {
-  id: number;
-  nome: string;
-  email: string;
-};
 
 
 export default function Dashboard() {
-    const {nfePermission,ctePermission, nfsePermission, userPermission, profilePermission, entrancePermission} = useSecurityContext()
-    const time = new Date().toLocaleString()
+    const {
+      nfePermission,
+      ctePermission, 
+      nfsePermission, 
+      userPermission, 
+      profilePermission, 
+      entrancePermission} 
+      = useSecurityContext()
+    const [nfeAmount, setNfeAmount] = useState(0)
+    const [cteAmount, setCteAmount] = useState(0)
+    const [nfseAmount, setNfseAmount] = useState(0)
+    const [totalAmount, setTotalAmount] = useState(0)
+    const [totalUsersAmount, setTotalUsersAmount] = useState(0)
+    const [firstPercentage, setFirstPercentage] = useState(0)
+    const [secondtPercentage, setSecondPercentage] = useState(0)
+    const [totalUsers, setTotalUsers] = useState(10)
+    const [accountName, setAccountName] = useState("")
+    const [accountDescription, setAccountDescription] = useState("")
+    const [totalValue, setTotalValue] = useState(10)
+    const [session] = useSession()
+    
+
+    const chartStyle = {
+      height: 50,
+    }
+
+      // request de plano
+    const getAccountData = useCallback(async () => {
+      const response = await planos.getAccountById(Number(session?.usuario.empresa.plano.id))
+      const data = response.data
+      console.log("teste",data)
+      return data
+      },[])
+
+      // request de dashboard data
+    const getDashboardData = useCallback(async() => {
+      const response = await empresas.dashboardRequest(Number(session?.usuario.empresa.id))
+      const data = response.data
+      console.log("dash", data)
+      return data
+      },[])
+    
+    
+    function getPercentage(partial: number, total: number) {
+      return partial / total
+    }
+
+    useEffect(() => {
+      getAccountData().then(response => {setTotalValue(response.notas), 
+        setTotalUsers(response.usuarios),
+        setAccountName(response.nome),
+        setAccountDescription(response.descricao)})
+      getDashboardData().then(response =>  {setTotalAmount(response.notas), 
+        setTotalUsersAmount(response.usuarios),
+        setCteAmount(response.CteCount), 
+        setNfseAmount(response.NfseCount), 
+        setNfeAmount(response.NfeCount)})
+      const firstSpeedometer = getPercentage(totalAmount, totalValue)
+      const secondSpeedometer = getPercentage(totalUsersAmount, totalUsers)
+      setFirstPercentage(firstSpeedometer)
+      setSecondPercentage(secondSpeedometer)
+      console.log("teste")
+    }, [totalValue, totalAmount, totalUsers, totalUsersAmount])
  
  
  
 
     return  <>
-    <Head>
-        <title>Orion | Dashboard</title>
-    </Head>
-    <h2>Dashboard</h2>
-
-    <SelectStyle>
-  <select title="Ano" name="Ano" id="ano">
-    <option value="Ano"  >Ano</option>
-    <option value="2010" >2010</option>
-  </select>
-  <select title="Mês" name="Mês" id="mes">
-    <option value="" disabled >Mês</option>
-    <option value="Janeiro" >2010</option>
-  </select>
-  </SelectStyle>
-  <div style={{height: "35vh"}}>
+        <Head>
+            <title>Orion | Dashboard</title>
+        </Head>
+        <h2>Dashboard</h2>
+        <InfoContainer>
+          <div>
+            <div>
+              <h3>NF-e</h3>
+              <h4>{nfeAmount}</h4>
+              <BsChevronCompactUp />
+            </div>
+            <div>
+              <h3>CT-e</h3>
+              <h4>{cteAmount}</h4>
+              <BsChevronCompactUp />
+            </div>
+            <div>
+              <h3>NFS-e</h3>
+              <h4>{nfseAmount}</h4>
+              <BsChevronCompactUp />
+              </div>
+          </div>
+          <div>
+            <div>
+              <h2>{accountName}</h2>
+              <p>{accountDescription}</p>
+            </div>
+          </div>
+        </InfoContainer>
+       
+  {/* <div style={{height: "35vh"}}>
   {nfePermission && <h3>Nf-e</h3>}
   {ctePermission && <h3>Ct-e</h3>}
   {nfsePermission &&  <h3>Nfs-e</h3>}
   {entrancePermission && <h3>Portaria</h3>}
   {profilePermission && <h3>Perfis</h3>}
   {userPermission && <h3>Usuários</h3>}
-  </div>
-<Speedometer>
-  <div id="main-div">
-  {/* velocímetro 1  */}
-  <div className="layout-align">
-    <div id="score-meter-1" className="layout-align">
-    <div id="scorer-1-inner-div">
-      <div id="scorer-1-inner-div-5">
-        <div className="scorer-1-tick">
-        </div>
-      </div>
-    </div>
-    <div id="scorer-1-inner-div-2"></div>
-    <div id="scorer-1-inner-div-3"></div>
-    <div id="scorer-1-inner-div-4"></div>
-  </div>
-  </div>
-  </div>
-  <div id="main-div">
-  {/* velocímetro 2 */}
-  <div className="layout-align">
-    <div id="score-meter-1" className="layout-align">
-    <div id="scorer-1-inner-div">
-      <div id="scorer-1-inner-div-5">
-        <div className="scorer-1-tick">
-        </div>
-      </div>
-    </div>
-    <div id="scorer-1-inner-div-2"></div>
-    <div id="scorer-1-inner-div-3"></div>
-    <div id="scorer-1-inner-div-4"></div>
-  </div>
-  </div>
-  </div>
-  </Speedometer>
-    
+  </div> */}
+        <Speedometer>
+          <div>
+            <h3>Notas</h3>
+            <GaugeChart arcWidth={0.1} 
+              nrOfLevels={38} percent={firstPercentage} style={chartStyle} animateDuration={500}
+              id="gauge-chart1" hideText/>
+              <span>
+                <h5>{totalAmount} / {totalValue}</h5>
+              </span>
+          </div>
+          <div>
+            <h3>Usuários</h3>
+            <GaugeChart arcWidth={0.1} style={chartStyle}
+              nrOfLevels={38} percent={secondtPercentage} animateDuration={1500}
+              id="gauge-chart2" hideText/> 
+              <span>
+                <h5>{totalUsersAmount} / {totalUsers}</h5>
+              </span> 
+          </div>
+        </Speedometer>
     </>
 }
 
