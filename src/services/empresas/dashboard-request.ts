27ce41a224388ae934519a9aa363436dcb35e255
TauDuque <hectauduque@gmail.com>
2021-11-19 10:38:07 -0300

    services to other pastes

--- src/services/empresas/dashboard-request.ts
+++
@@ -1,6 +0,0 @@
-import api from "@services/api";
-
-export default async function dashboardRequest(id: number){
-    const response = await api.get(`/empresas/dashboard/${id}`)
-    return response
-}
--- src/services/empresas/delete.ts
+++
@@ -1,6 +0,0 @@
-import api from "@services/api";
-
-export default async function deletar(id: number) {
-    const response = await api.delete(`empresas/${id}`);
-    return response
-}
