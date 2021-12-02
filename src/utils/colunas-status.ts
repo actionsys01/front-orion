0a6e8a737b41165854285bba735b896ef51588e2
TauDuque <hectauduque@gmail.com>
2021-12-02 11:17:30 -0300

    filterfinal pt IV

---
+++ src/utils/colunas-status.ts
@@ -0,0 +1,7 @@
+export default [
+    { value: "0", label: "Na Portaria" },
+    { value: "1", label: "Entrada Autorizada" },
+    { value: "2", label: "Entrada Fechada" },
+    { value: "3", label: "Não se Aplica" },
+    { value: "4", label: "Entrega Cancelada" }
+]
