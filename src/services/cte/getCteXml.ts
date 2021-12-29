b356769d336932fb93b0b695e31f942ed2be3806
TauDuque <hectauduque@gmail.com>
2021-12-29 17:41:39 -0300

    request update and logo handler

--- src/services/cte/getCteXml.ts
+++ src/services/cte/getCteXml.ts
@@ -1,6 +1,7 @@
 import api from "@services/api";
 
 export default async function getCteXml(chave :  string) {
+    // console.log(`chave`, chave)
     const response = await api.get(`/ctes/informations/${chave}`)
 
     return response
