f9f86fd6951f2caf960efcaa7ace60a331de8ba3
TauDuque <hectauduque@gmail.com>
2021-12-29 18:00:25 -0300

    logout debug pt 99

--- src/components/MenuLateral/index.tsx
+++ src/components/MenuLateral/index.tsx
@@ -55,7 +55,7 @@ export default function MenuLateral({
   const isMD = useMediaQuery("lg");
 
   async function sair() {
-    await limpar();
+    limpar();
     await signOut({
       callbackUrl: "https://gra-web.netlify.app",
     });
