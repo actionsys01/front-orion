5267ae513f9ea2576c2b48ec0cdda20cbf5f3a3b
TauDuque <hectauduque@gmail.com>
2022-02-07 09:23:44 -0300

    contexts with eslint format

--- src/contexts/ControlFilter.tsx
+++ src/contexts/ControlFilter.tsx
@@ -1,85 +1,81 @@
-import { useEffect,  Dispatch, SetStateAction } from "react";
-import { useContext, useState } from "react";
-import { createContext } from "react";
-import colunas from "@utils/filtros-controle";
-import { ReactNode } from "hoist-non-react-statics/node_modules/@types/react";
+import { useEffect, Dispatch, SetStateAction } from 'react';
+import { useContext, useState } from 'react';
+import { createContext } from 'react';
+import colunas from '@utils/filtros-controle';
+import { ReactNode } from 'hoist-non-react-statics/node_modules/@types/react';
 
 interface IUnform {
-  campo: string
-  valor: string
+    campo: string;
+    valor: string;
 }
 interface IFiltro {
-  campo : string
-  valor: string
+    campo: string;
+    valor: string;
 }
 
-
-
 interface FiltroContextType {
-  filters: IFiltro[];
-  registerFilter( filters: IFiltro[]): void
-  scopeIgnition(filtro: IUnform[]): IFiltro[];
-  limpar(): void;
+    filters: IFiltro[];
+    registerFilter(filters: IFiltro[]): void;
+    scopeIgnition(filtro: IUnform[]): IFiltro[];
+    limpar(): void;
 }
 
 interface FilterProps {
-  [key: string] : string;
+    [key: string]: string;
 }
 
-interface IFiltroProps{
-  children : ReactNode
+interface IFiltroProps {
+    children: ReactNode;
 }
 
 export const ControlFilterContext = createContext({} as FiltroContextType);
 
 export function useControlFilter() {
-  return useContext(ControlFilterContext);
+    return useContext(ControlFilterContext);
 }
 
-export default function ControlFilterProvider({ children } : IFiltroProps) {
-  const [filters, setFilters] = useState<IFiltro[]>([]);
+export default function ControlFilterProvider({ children }: IFiltroProps) {
+    const [filters, setFilters] = useState<IFiltro[]>([]);
 
+    useEffect(() => {
+        async function loadStorage() {
+            const controlStorage = localStorage.getItem('filtersObj');
+            if (controlStorage) setFilters(JSON.parse(controlStorage));
+        }
+        // console.log(`filters no context`, filters)
+        loadStorage();
+    }, []);
 
-  useEffect(() => {
-    async function loadStorage() {
-      const controlStorage = localStorage.getItem("filtersObj");
-      if (controlStorage) setFilters(JSON.parse(controlStorage));
+    function registerFilter(filters: IFiltro[]) {
+        setFilters(filters);
+        localStorage.setItem('filtersObj', JSON.stringify(filters));
     }
-    // console.log(`filters no context`, filters)
-    loadStorage();
-  }, []);
 
+    function scopeIgnition(array: IUnform[]): IFiltro[] {
+        const filtro = array.map(({ campo, valor }) => {
+            const coluna = colunas.find(option => option.value === campo);
+            if (coluna) {
+                return { campo, valor };
+            }
+        });
 
-  function registerFilter(filters: IFiltro[]) {
-    setFilters(filters);
-    localStorage.setItem("filtersObj", JSON.stringify(filters));
-  }
+        return filtro;
+    }
 
-  function scopeIgnition(array: IUnform[]): IFiltro[] {
-    let filtro = array.map(({campo, valor}) => {
-      const coluna =  colunas.find((option) => option.value === campo)
-      if(coluna) {
-        return { campo ,valor}
-      }
-    });
+    async function limpar() {
+        localStorage.setItem('filtersObj', JSON.stringify([]));
+    }
 
-    return filtro;
-  }
-
-  async function limpar() {
-    localStorage.setItem("filtersObj", JSON.stringify([]));
-  }
-
-  return (
-    <ControlFilterContext.Provider
-      value={{
-        filters,
-        registerFilter,
-        scopeIgnition,
-        limpar,
-      }}
-    >
-      {children}
-    </ControlFilterContext.Provider>
-  );
+    return (
+        <ControlFilterContext.Provider
+            value={{
+                filters,
+                registerFilter,
+                scopeIgnition,
+                limpar,
+            }}
+        >
+            {children}
+        </ControlFilterContext.Provider>
+    );
 }
--- src/contexts/filtro.tsx
+++ src/contexts/filtro.tsx
@@ -1,126 +1,124 @@
-import { useEffect,  Dispatch, SetStateAction } from "react";
-import { useContext, useState } from "react";
-import { createContext } from "react";
-import colunas from "@utils/painel-controle-filtro";
-import nfse_colunas from "@utils/controle-nfse-filtros"
-import { ReactNode } from "hoist-non-react-statics/node_modules/@types/react";
+import { useEffect, Dispatch, SetStateAction } from 'react';
+import { useContext, useState } from 'react';
+import { createContext } from 'react';
+import colunas from '@utils/painel-controle-filtro';
+import nfse_colunas from '@utils/controle-nfse-filtros';
+import { ReactNode } from 'hoist-non-react-statics/node_modules/@types/react';
 
 interface IUnform {
-  campo: string;
-  valor: string;
+    campo: string;
+    valor: string;
 }
 interface IFiltro {
-  campo: { label: string; value: string } | undefined;
-  valor: string;
+    campo: { label: string; value: string } | undefined;
+    valor: string;
 }
 
 interface FiltroContextType {
-  nfes: IFiltro[];
-  ctes: IFiltro[];
-  nfses: IUnform[];
-  cadastrarNfe(nfes: IFiltro[]): void;
-  cadastrarCte(ctes: IFiltro[]): void;
-  cadastrarNfse(nfses: IUnform[]): void;
-  inicializarScope(filtro: IUnform[]): IFiltro[];
-  scopeIgnition(filtro: IUnform[]): IUnform[]; 
-  limpar(): void;
+    nfes: IUnform[];
+    ctes: IFiltro[];
+    nfses: IUnform[];
+    cadastrarNfe(nfes: IUnform[]): void;
+    cadastrarCte(ctes: IFiltro[]): void;
+    cadastrarNfse(nfses: IUnform[]): void;
+    inicializarScope(filtro: IUnform[]): IFiltro[];
+    scopeIgnition(filtro: IUnform[]): IUnform[];
+    limpar(): void;
 }
 
-interface IFiltroProps{
-  children : ReactNode
+interface IFiltroProps {
+    children: ReactNode;
 }
 
 export const FiltroContext = createContext({} as FiltroContextType);
 
 export function useFiltro() {
-  return useContext(FiltroContext);
+    return useContext(FiltroContext);
 }
 
-export default function FiltroProvider({ children } : IFiltroProps) {
-  const [nfes, setNfes] = useState<IFiltro[]>([]);
-  const [ctes, setCtes] = useState<IFiltro[]>([]);
-  const [nfses, setNfses] = useState<IUnform[]>([])
+export default function FiltroProvider({ children }: IFiltroProps) {
+    const [nfes, setNfes] = useState<IUnform[]>([]);
+    const [ctes, setCtes] = useState<IFiltro[]>([]);
+    const [nfses, setNfses] = useState<IUnform[]>([]);
 
-  useEffect(() => {
-    async function carregarStorage() {
-      const nfesStorage = localStorage.getItem("@orions:nfes");
-      const ctesStorage = localStorage.getItem("@orion:ctes");
-      const nfsesStorage = localStorage.getItem("@orions:nfses");
-      if (nfesStorage) setNfes(JSON.parse(nfesStorage));
-      if (ctesStorage) setCtes(JSON.parse(ctesStorage));
-      if (nfsesStorage) setNfses(JSON.parse(nfsesStorage));
+    useEffect(() => {
+        async function carregarStorage() {
+            const nfesStorage = localStorage.getItem('@orions:nfes');
+            const ctesStorage = localStorage.getItem('@orion:ctes');
+            const nfsesStorage = localStorage.getItem('@orions:nfses');
+            if (nfesStorage) setNfes(JSON.parse(nfesStorage));
+            if (ctesStorage) setCtes(JSON.parse(ctesStorage));
+            if (nfsesStorage) setNfses(JSON.parse(nfsesStorage));
+        }
+
+        carregarStorage();
+    }, []);
+
+    // useEffect(() => {
+
+    //   console.log("FILTRO" , nfes)
+
+    // }, [nfes])
+
+    function cadastrarNfe(nfes: IUnform[]) {
+        setNfes(nfes);
+        localStorage.setItem('@orions:nfes', JSON.stringify(nfes));
     }
 
-    carregarStorage();
-  }, []);
+    function cadastrarCte(ctes: IFiltro[]) {
+        setCtes(ctes);
+        localStorage.setItem('@orion:ctes', JSON.stringify(ctes));
+    }
 
+    function cadastrarNfse(nfses: IUnform[]) {
+        setNfses(nfses);
+        localStorage.setItem('@orions:nfses', JSON.stringify(nfses));
+    }
 
+    function inicializarScope(array: IUnform[]): IFiltro[] {
+        const filtro: IFiltro[] = [];
 
-  // useEffect(() => {
+        array.forEach(item => {
+            const { campo, valor } = item;
+            const coluna = colunas.find(option => option.value === campo);
+            filtro.push({ campo: coluna, valor: valor });
+        });
 
-  //   console.log("FILTRO" , nfes)
+        return filtro;
+    }
 
-  // }, [nfes])
+    function scopeIgnition(array: IUnform[]): IUnform[] {
+        const filtro = array.map(({ campo, valor }) => {
+            const coluna = nfse_colunas.find(option => option.value === campo);
+            if (coluna) {
+                return { campo, valor };
+            }
+        });
 
-  function cadastrarNfe(nfes: IFiltro[]) {
-    setNfes(nfes);
-    localStorage.setItem("@orions:nfes", JSON.stringify(nfes));
-  }
+        return filtro;
+    }
 
-  function cadastrarCte(ctes: IFiltro[]) {
-    setCtes(ctes);
-    localStorage.setItem("@orion:ctes", JSON.stringify(ctes));
-  }
+    async function limpar() {
+        localStorage.setItem('@orion:ctes', JSON.stringify([]));
+        localStorage.setItem('@orions:nfes', JSON.stringify([]));
+        localStorage.setItem('@orions:nfses', JSON.stringify([]));
+    }
 
-  function cadastrarNfse(nfses: IUnform[]){
-    setNfses(nfses)
-    localStorage.setItem("@orions:nfses", JSON.stringify(nfses));
-  }
-
-  function inicializarScope(array: IUnform[]): IFiltro[] {
-    let filtro: IFiltro[] = [];
-
-    array.forEach((item) => {
-      const { campo, valor } = item;
-      const coluna = colunas.find((option) => option.value === campo);
-      filtro.push({ campo: coluna, valor: valor });
-    });
-
-    return filtro;
-  }
-
-  function scopeIgnition(array: IUnform[]): IUnform[] {
-    let filtro = array.map(({campo, valor}) => {
-      const coluna =  nfse_colunas.find((option) => option.value === campo)
-      if(coluna) {
-        return { campo ,valor}
-      }
-    });
-
-    return filtro;
-  }
-
-  async function limpar() {
-    localStorage.setItem("@orion:ctes", JSON.stringify([]));
-    localStorage.setItem("@orions:nfes", JSON.stringify([]));
-    localStorage.setItem("@orions:nfses", JSON.stringify([]));
-  }
-
-  return (
-    <FiltroContext.Provider
-      value={{
-        nfes,
-        ctes,
-        nfses,
-        cadastrarCte,
-        cadastrarNfe,
-        cadastrarNfse,
-        inicializarScope,
-        scopeIgnition,
-        limpar,
-      }}
-    >
-      {children}
-    </FiltroContext.Provider>
-  );
+    return (
+        <FiltroContext.Provider
+            value={{
+                nfes,
+                ctes,
+                nfses,
+                cadastrarCte,
+                cadastrarNfe,
+                cadastrarNfse,
+                inicializarScope,
+                scopeIgnition,
+                limpar,
+            }}
+        >
+            {children}
+        </FiltroContext.Provider>
+    );
 }
