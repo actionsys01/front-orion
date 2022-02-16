4bf359aeb8c0027b8046fd9aa50f22c71afc146a
TauDuque <hectauduque@gmail.com>
2022-02-16 11:22:09 -0300

    subj: eslint fix and label dot adjusted nfe

--- src/components/CtePagination/Popover/index.tsx
+++ src/components/CtePagination/Popover/index.tsx
@@ -7,149 +7,140 @@ import getCteXml from '@services/cte/getCteXml';
 import Dacte from '@components/dacte';
 
 interface PopoverProps {
-    item: any;
+  item: any;
 }
 
 const PopoverCte: React.FC<PopoverProps> = ({ item }) => {
-    const [visible, setVisible] = useState(false);
-    // const [secondPopoverVisible, setSecondPopoverVisible] = useState(false)
-    const { ctePermissions } = useSecurityContext();
-    const [, setToast] = useToasts();
+  const [visible, setVisible] = useState(false);
+  // const [secondPopoverVisible, setSecondPopoverVisible] = useState(false)
+  const { ctePermissions } = useSecurityContext();
+  const [, setToast] = useToasts();
 
-    const changeHandler = useCallback(next => {
-        setVisible(next);
-    }, []);
+  const changeHandler = useCallback(next => {
+    setVisible(next);
+  }, []);
 
-    // const changeHandlerSecondPopover = useCallback((next) => {
-    //   setSecondPopoverVisible(next)
-    // }, [])
+  // const changeHandlerSecondPopover = useCallback((next) => {
+  //   setSecondPopoverVisible(next)
+  // }, [])
 
-    const printData = useCallback(async chave_nota => {
-        const cteData: any = [];
-        const medidasArray: any = [];
-        const produtosArray: any = [];
-        try {
-            const response = await getCteXml(chave_nota);
-            const cteResponse = response.data;
-            if (Array.isArray(cteResponse)) {
-                const medidas = cteResponse.map(
-                    item => item.informacoes_normal_substituto.infCarga.infQ,
-                );
-                const produtos = cteResponse.map(
-                    item => item.valores_servicos.Comp,
-                );
-                Dacte(cteResponse, medidas, produtos, chave_nota);
-            } else {
-                cteData.push(cteResponse);
-                const medidas =
-                    cteResponse.informacoes_normal_substituto.infCarga.infQ;
-                const produtos = cteResponse.valores_servicos.Comp;
-                // console.log("etapa 1", medidas)
-                if (Array.isArray(medidas) && Array.isArray(produtos)) {
-                    Dacte(cteData, medidas, produtos, chave_nota);
-                } else {
-                    medidasArray.push(medidas);
-                    produtosArray.push(produtos);
-                    // console.log("etapa 2",medidas)
-                    Dacte(cteData, medidasArray, produtosArray, chave_nota);
-                }
-            }
-        } catch (error) {
-            console.log(error);
-            setToast({
-                text: 'Houve um problema, por favor tente novamente',
-                type: 'warning',
-            });
+  const printData = useCallback(async chave_nota => {
+    const cteData: any = [];
+    const medidasArray: any = [];
+    const produtosArray: any = [];
+    try {
+      const response = await getCteXml(chave_nota);
+      const cteResponse = response.data;
+      if (Array.isArray(cteResponse)) {
+        const medidas = cteResponse.map(
+          item => item.informacoes_normal_substituto.infCarga.infQ,
+        );
+        const produtos = cteResponse.map(item => item.valores_servicos.Comp);
+        Dacte(cteResponse, medidas, produtos, chave_nota);
+      } else {
+        cteData.push(cteResponse);
+        const medidas = cteResponse.informacoes_normal_substituto.infCarga.infQ;
+        const produtos = cteResponse.valores_servicos.Comp;
+        // console.log("etapa 1", medidas)
+        if (Array.isArray(medidas) && Array.isArray(produtos)) {
+          Dacte(cteData, medidas, produtos, chave_nota);
+        } else {
+          medidasArray.push(medidas);
+          produtosArray.push(produtos);
+          // console.log("etapa 2",medidas)
+          Dacte(cteData, medidasArray, produtosArray, chave_nota);
         }
-    }, []);
+      }
+    } catch (error) {
+      console.log(error);
+      setToast({
+        text: 'Houve um problema, por favor tente novamente',
+        type: 'warning',
+      });
+    }
+  }, []);
 
-    return (
-        <>
-            <Popover
-                visible={visible}
-                onVisibleChange={changeHandler}
-                placement="right"
-                content={
-                    <>
-                        <Popover.Item>
-                            <Text
-                                style={{ cursor: 'pointer' }}
-                                onClick={() => {
-                                    const chave_nota = item.rowValue.chave_nota;
-                                    const status_sefaz = Number(
-                                        item.rowValue.sefaz_status,
-                                    );
-                                    const desc_status_sefaz =
-                                        item.rowValue.sefaz_status_desc;
-                                    // console.log(item);
-                                    router.push({
-                                        pathname: '/cte-detalhes',
-                                        query: {
-                                            chave_nota,
-                                            status_sefaz,
-                                            desc_status_sefaz,
-                                        },
-                                    });
-                                }}
-                            >
-                                Visualizar
-                            </Text>
-                        </Popover.Item>
-                        <Popover.Item>
-                            <a href={item?.rowValue.xml} download>
-                                <Text
-                                    onClick={() => setVisible(false)}
-                                    style={{
-                                        cursor: 'pointer',
-                                        textDecoration: 'none',
-                                        color: '#1C496A',
-                                        margin: '0',
-                                    }}
-                                >
-                                    Download
-                                </Text>
-                            </a>
-                        </Popover.Item>
-                        {ctePermissions.HISTORICO && (
-                            <Popover.Item>
-                                <Text
-                                    style={{ cursor: 'pointer' }}
-                                    onClick={() => {
-                                        const chave_nota =
-                                            item?.rowValue.chave_nota;
-                                        const empresa_id =
-                                            item?.rowValue.empresa_id;
-                                        router.push({
-                                            pathname: `/historico-notas`,
-                                            query: { chave_nota, empresa_id },
-                                        });
-                                    }}
-                                >
-                                    Histórico de nota
-                                </Text>
-                            </Popover.Item>
-                        )}
-                        <Popover.Item>
-                            <Text
-                                style={{ cursor: 'pointer' }}
-                                onClick={() => {
-                                    const chave_nota =
-                                        item?.rowValue.chave_nota;
-                                    printData(chave_nota);
-                                }}
-                            >
-                                Imprimir Nota
-                            </Text>
-                        </Popover.Item>
-                    </>
-                }
-            >
-                <span style={{ cursor: 'pointer' }}>
-                    <MoreHorizontal />
-                </span>
-            </Popover>
-        </>
-    );
+  return (
+    <>
+      <Popover
+        visible={visible}
+        onVisibleChange={changeHandler}
+        placement="right"
+        content={
+          <>
+            <Popover.Item>
+              <Text
+                style={{ cursor: 'pointer' }}
+                onClick={() => {
+                  const chave_nota = item.rowValue.chave_nota;
+                  const status_sefaz = Number(item.rowValue.sefaz_status);
+                  const desc_status_sefaz = item.rowValue.sefaz_status_desc;
+                  // console.log(item);
+                  router.push({
+                    pathname: '/cte-detalhes',
+                    query: {
+                      chave_nota,
+                      status_sefaz,
+                      desc_status_sefaz,
+                    },
+                  });
+                }}
+              >
+                Visualizar
+              </Text>
+            </Popover.Item>
+            <Popover.Item>
+              <a href={item?.rowValue.xml} download>
+                <Text
+                  onClick={() => setVisible(false)}
+                  style={{
+                    cursor: 'pointer',
+                    textDecoration: 'none',
+                    color: '#1C496A',
+                    margin: '0',
+                  }}
+                >
+                  Download
+                </Text>
+              </a>
+            </Popover.Item>
+            {ctePermissions.HISTORICO && (
+              <Popover.Item>
+                <Text
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => {
+                    const chave_nota = item?.rowValue.chave_nota;
+                    const empresa_id = item?.rowValue.empresa_id;
+                    router.push({
+                      pathname: `/historico-notas`,
+                      query: { chave_nota, empresa_id },
+                    });
+                  }}
+                >
+                  Histórico de nota
+                </Text>
+              </Popover.Item>
+            )}
+            <Popover.Item>
+              <Text
+                style={{ cursor: 'pointer' }}
+                onClick={() => {
+                  const chave_nota = item?.rowValue.chave_nota;
+                  printData(chave_nota);
+                }}
+              >
+                Imprimir Nota
+              </Text>
+            </Popover.Item>
+          </>
+        }
+      >
+        <span style={{ cursor: 'pointer' }}>
+          <MoreHorizontal />
+        </span>
+      </Popover>
+    </>
+  );
 };
 
 export default PopoverCte;
--- src/components/NfePagination/index.tsx
+++ src/components/NfePagination/index.tsx
@@ -15,197 +15,165 @@ import { useToasts } from '@geist-ui/react';
 import Loader from '@components/Loader';
 
 interface Props {
-    company_id: number;
-    sefaz?: {
-        cor: 'secondary' | 'success' | 'error' | 'warning' | 'default';
-        message: string;
-    };
-    portaria?: {
-        cor: 'success' | 'warning' | 'default' | 'error';
-        message: string;
-    };
+  company_id: number;
+  sefaz?: {
+    cor: 'secondary' | 'success' | 'error' | 'warning' | 'default';
+    message: string;
+  };
+  portaria?: {
+    cor: 'success' | 'warning' | 'default' | 'error';
+    message: string;
+  };
 }
 
 export default function NfePagination({ company_id, sefaz, portaria }: Props) {
-    const [nfe, setNfes] = useState<INfeDto[]>([]);
-    const { nfes } = useFiltro();
-    const [page, setPage] = useState(1);
-    const [quantityPage, setQuantityPage] = useState(1);
-    const { nfePermissions } = useSecurityContext();
-    const [, setToast] = useToasts();
-    const [loading, setLoading] = useState(true);
+  const [nfe, setNfes] = useState<INfeDto[]>([]);
+  const { nfes } = useFiltro();
+  const [page, setPage] = useState(1);
+  const [quantityPage, setQuantityPage] = useState(1);
+  const { nfePermissions } = useSecurityContext();
+  const [, setToast] = useToasts();
+  const [loading, setLoading] = useState(true);
 
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value);
-    };
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
-    const getNfesAndTotalPages = useCallback(async () => {
-        try {
-            const responseNfes = await getNfePagesByCompanyId(
-                page,
-                Number(company_id),
-                nfes,
-            );
-            const { data } = responseNfes;
-            setLoading(false);
-            setNfes(data.nfes);
-            setQuantityPage(Math.ceil(data.total / 8));
-        } catch (error) {
-            setToast({
-                text: 'Notas não localizadas',
-                type: 'warning',
-            });
-        }
-    }, [nfes, page]);
-
-    useEffect(() => {
-        getNfesAndTotalPages();
-    }, [page, nfes]);
-
-    useEffect(() => {
-        if (page > quantityPage) {
-            setPage(1);
-        }
-    }, [nfes, quantityPage, page]);
-
-    // console.log('nfe.controle_entrada', nfe);
-
-    const dataFormatted = useMemo(() => {
-        const newData: any = [];
-        if (nfe) {
-            nfe.forEach(item => {
-                newData.push({
-                    ...item,
-                    sefaz_status: (
-                        <Tooltip
-                            text={
-                                item?.sefaz_status === 999
-                                    ? 'Indisponível'
-                                    : item?.sefaz_status === 100
-                                    ? 'Autorizada'
-                                    : item?.sefaz_status === 101
-                                    ? 'Cancelada'
-                                    : 'null'
-                            }
-                            type={sefaz?.cor}
-                        >
-                            <Dot
-                                type={
-                                    item?.sefaz_status === 100
-                                        ? 'success'
-                                        : item?.sefaz_status === 101
-                                        ? 'warning'
-                                        : 'default'
-                                }
-                            />
-                        </Tooltip>
-                    ),
-                    portaria_status: (
-                        <Tooltip
-                            text={
-                                item?.controle_entrada?.status === 0
-                                    ? 'Na Portaria'
-                                    : item?.controle_entrada?.status === 1
-                                    ? 'Entrada Autorizada'
-                                    : item?.controle_entrada?.status === 2
-                                    ? 'Entrada Fechada'
-                                    : item?.controle_entrada?.status === 3
-                                    ? 'Não se Aplica'
-                                    : item?.controle_entrada?.status === 4
-                                    ? 'Entrega Cancelada'
-                                    : 'Aguardando Chegada'
-                            }
-                            type={portaria?.cor}
-                        >
-                            <Dot
-                                type={
-                                    item?.controle_entrada?.status === 0
-                                        ? 'warning'
-                                        : item?.controle_entrada?.status === 1
-                                        ? 'success'
-                                        : item?.controle_entrada?.status === 2
-                                        ? 'success'
-                                        : item?.controle_entrada?.status === 3
-                                        ? 'error'
-                                        : item?.controle_entrada?.status === 4
-                                        ? 'error'
-                                        : 'default'
-                                }
-                            />
-                        </Tooltip>
-                    ),
-                    emissionDate: format(
-                        new Date(item.dt_hr_emi),
-                        'dd/MM/yyyy HH:mm:ss',
-                    ),
-                    receiveDate: format(
-                        new Date(item.criado_em),
-                        'dd/MM/yyyy HH:mm:ss',
-                    ),
-                    option: (actions: any, item: any) => (
-                        <PopoverComponent item={item} />
-                    ),
-                });
-            });
-        }
-        return newData;
-    }, [nfe]);
-
-    if (loading) {
-        return <Loader />;
+  const getNfesAndTotalPages = useCallback(async () => {
+    try {
+      const responseNfes = await getNfePagesByCompanyId(
+        page,
+        Number(company_id),
+        nfes,
+      );
+      const { data } = responseNfes;
+      setLoading(false);
+      setNfes(data.nfes);
+      setQuantityPage(Math.ceil(data.total / 8));
+    } catch (error) {
+      setToast({
+        text: 'Notas não localizadas',
+        type: 'warning',
+      });
     }
+  }, [nfes, page]);
 
-    return (
-        <>
-            <Grid>
-                {nfePermissions.VISUALIZAR && (
-                    <Table data={dataFormatted}>
-                        <Table.Column prop="option" />
-                        <Table.Column
-                            prop="emissionDate"
-                            label="Data/hora Emissão"
-                        />
-                        <Table.Column prop="nota" label="Número" />
-                        <Table.Column prop="serie" label="Série" />
-                        <Table.Column
-                            prop="emit_cnpj"
-                            label="CNPJ Fornecedor"
-                        />
-                        <Table.Column
-                            prop="emit_nome"
-                            label="Nome Fornecedor"
-                        />
-                        <Table.Column
-                            prop="sefaz_status"
-                            label="Status Sefaz"
-                        />
-                        <Table.Column
-                            prop="portaria_status"
-                            label="Status Entrada"
-                        />
-                        <Table.Column
-                            prop="chave_nota"
-                            label="Chave de Acesso"
-                        />
-                        <Table.Column
-                            prop="dest_cnpj"
-                            label="CNPJ Destinatário"
-                        />
-                        <Table.Column prop="dest_nome" label="Destinatário" />
-                        <Table.Column
-                            prop="receiveDate"
-                            label="Data/hora Recebimento"
-                        />
-                    </Table>
-                )}
-            </Grid>
-            <Pages>
-                <Pagination
-                    onChange={handleChange}
-                    count={quantityPage}
-                    shape="rounded"
-                />
-            </Pages>
-        </>
-    );
+  useEffect(() => {
+    getNfesAndTotalPages();
+  }, [page, nfes]);
+
+  useEffect(() => {
+    if (page > quantityPage) {
+      setPage(1);
+    }
+  }, [nfes, quantityPage, page]);
+
+  // console.log('nfe.controle_entrada', nfe);
+
+  const dataFormatted = useMemo(() => {
+    const newData: any = [];
+    if (nfe) {
+      nfe.forEach(item => {
+        newData.push({
+          ...item,
+          sefaz_status: (
+            <Tooltip
+              text={
+                item?.sefaz_status === 999
+                  ? 'Indisponível'
+                  : item?.sefaz_status === 100
+                  ? 'Autorizada'
+                  : item?.sefaz_status === 101
+                  ? 'Cancelada'
+                  : 'Indisponível'
+              }
+              type={sefaz?.cor}
+            >
+              <Dot
+                type={
+                  item?.sefaz_status === 100
+                    ? 'success'
+                    : item?.sefaz_status === 101
+                    ? 'warning'
+                    : 'default'
+                }
+              />
+            </Tooltip>
+          ),
+          portaria_status: (
+            <Tooltip
+              text={
+                item?.controle_entrada?.status === 0
+                  ? 'Na Portaria'
+                  : item?.controle_entrada?.status === 1
+                  ? 'Entrada Autorizada'
+                  : item?.controle_entrada?.status === 2
+                  ? 'Entrada Fechada'
+                  : item?.controle_entrada?.status === 3
+                  ? 'Não se Aplica'
+                  : item?.controle_entrada?.status === 4
+                  ? 'Entrega Cancelada'
+                  : 'Aguardando Chegada'
+              }
+              type={portaria?.cor}
+            >
+              <Dot
+                type={
+                  item?.controle_entrada?.status === 0
+                    ? 'warning'
+                    : item?.controle_entrada?.status === 1
+                    ? 'success'
+                    : item?.controle_entrada?.status === 2
+                    ? 'success'
+                    : item?.controle_entrada?.status === 3
+                    ? 'error'
+                    : item?.controle_entrada?.status === 4
+                    ? 'error'
+                    : 'default'
+                }
+              />
+            </Tooltip>
+          ),
+          emissionDate: format(new Date(item.dt_hr_emi), 'dd/MM/yyyy HH:mm:ss'),
+          receiveDate: format(new Date(item.criado_em), 'dd/MM/yyyy HH:mm:ss'),
+          option: (actions: any, item: any) => <PopoverComponent item={item} />,
+        });
+      });
+    }
+    return newData;
+  }, [nfe]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <Grid>
+        {nfePermissions.VISUALIZAR && (
+          <Table data={dataFormatted}>
+            <Table.Column prop="option" />
+            <Table.Column prop="emissionDate" label="Data/hora Emissão" />
+            <Table.Column prop="nota" label="Número" />
+            <Table.Column prop="serie" label="Série" />
+            <Table.Column prop="emit_cnpj" label="CNPJ Fornecedor" />
+            <Table.Column prop="emit_nome" label="Nome Fornecedor" />
+            <Table.Column prop="sefaz_status" label="Status Sefaz" />
+            <Table.Column prop="portaria_status" label="Status Entrada" />
+            <Table.Column prop="chave_nota" label="Chave de Acesso" />
+            <Table.Column prop="dest_cnpj" label="CNPJ Destinatário" />
+            <Table.Column prop="dest_nome" label="Destinatário" />
+            <Table.Column prop="receiveDate" label="Data/hora Recebimento" />
+          </Table>
+        )}
+      </Grid>
+      <Pages>
+        <Pagination
+          onChange={handleChange}
+          count={quantityPage}
+          shape="rounded"
+        />
+      </Pages>
+    </>
+  );
 }
