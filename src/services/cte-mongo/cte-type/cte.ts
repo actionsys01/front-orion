1e454f6608c4bc8f9636574f48578ede261698f3
TauDuque <hectauduque@gmail.com>
2022-02-22 13:49:53 -0300

    subj: type issues controle-entrada

--- src/services/cte-mongo/cte-type/cte.ts
+++ src/services/cte-mongo/cte-type/cte.ts
@@ -1,173 +1,202 @@
 export interface CteProps {
-    informacoes_cte: {
-        nCT: string;
-        serie: string;
-        dhEmi: Date;
-        dEmi: Date;
-    }
-    emitente: {
-        CNPJ: string;
-        xNome: string;
-    };
+  informacoes_cte: {
+    nCT: string;
+    serie: string;
+    dhEmi: Date;
+    dEmi: Date;
+  };
+  emitente: {
+    CNPJ: string;
+    xNome: string;
+  };
 
-    informacoes_noraml_substituto: {
-        infDoc: {
-            infNfe: {
-                chave: string
-            }
-        }
-    }
-    versao: string
+  informacoes_noraml_substituto: {
+    infDoc: {
+      infNfe: {
+        chave: string;
+      };
+    };
+  };
+  versao: string;
 }
 
 export interface CteXmlProps {
-        informacoes_cte: {
-            mod: string;
+  informacoes_cte: {
+    mod: string;
+    serie: string;
+    cCT: string;
+    nCT: string;
+    verProc: string;
+    dhEmi: string;
+    dEmi: string;
+    dhSaiEnt: string;
+    modal: string;
+    tpServ: string;
+    nCFOP: string;
+    tpEmis: string;
+    xMunFim: string;
+    UFFim: string;
+    UFIni: string;
+    xMunIni: string;
+  };
+  total: {
+    ICMSTot: { vNF: string };
+  };
+
+  emitente: {
+    xNome: string;
+    xFant: string;
+    CNPJ: string;
+    CNAE: string;
+    IM: string;
+    enderEmit: {
+      fone: string;
+      UF: string;
+      nro: string;
+      CEP: string;
+      xBairro: string;
+      xPais: string;
+      xMun: string;
+      xLgr: string;
+    };
+    IE: string;
+  };
+  remetente: {
+    xNome: string;
+    xFant: string;
+    CNPJ: string;
+    CNAE: string;
+    IM: string;
+    enderReme: {
+      fone: string;
+      UF: string;
+      nro: string;
+      CEP: string;
+      xBairro: string;
+      xPais: string;
+      xMun: string;
+      xLgr: string;
+    };
+    IE: string;
+  };
+  destinatario: {
+    xNome: string;
+    xFant: string;
+    CNPJ: string;
+    CNAE: string;
+    IM: string;
+    enderDest: {
+      fone: string;
+      UF: string;
+      nro: string;
+      CEP: string;
+      xBairro: string;
+      xPais: string;
+      xMun: string;
+      xLgr: string;
+    };
+    IE: string;
+  };
+  versao: string;
+  impostos: {
+    ICMS: {
+      ICMS00: { CST: string; vBC: string; vICMS: string; pICMS: string };
+      ICMS20: {
+        CST: string;
+        vBC: string;
+        pRedBC: string;
+        vICMS: string;
+        pICMS: string;
+      };
+      ICMS45: { CST: string };
+      ICMS60: {
+        CST: string;
+        vBCSTRet: string;
+        vICMSSTRet: string;
+        pICMSSTRet: string;
+      };
+      ICMS90: {
+        CST: string;
+        pRedBC: string;
+        vBC: string;
+        pICMS: string;
+        vICMS: string;
+      };
+      ICMSOutraUF: {
+        CST: string;
+        pRedBCOutraUF: string;
+        vBCOutraUF: string;
+        pICMSOutraUF: string;
+        vICMSOutraUF: string;
+      };
+      ICMSSN: {
+        CST: string;
+        indSN: string;
+        vTotTrib: string;
+        infAdFisco: string;
+      };
+    };
+  };
+  valores_servicos: {
+    vTPrest: string;
+    vRec: string;
+  };
+  sefaz: {
+    status: string;
+    descricao: string;
+  };
+  informacoes_normal_substituto: {
+    infCarga: {
+      vCarga: string;
+      proPred: string;
+      xOutCat: string;
+      infQ:
+        | [
+            {
+              tpMed: string;
+              cUnid: string;
+              qCarga: string;
+            },
+          ]
+        | {
+            tpMed: string;
+            cUnid: string;
+            qCarga: string;
+          };
+    };
+    infDoc: {
+      infNFe:
+        | [
+            {
+              chave: string;
+              nDoc: string;
+              serie: string;
+              dEmi: string;
+              vNF: string;
+            },
+          ]
+        | {
+            chave: string;
+            nDoc: string;
             serie: string;
-            cCT: string;
-            nCT: string;
-            verProc: string;
-            dhEmi: string;
             dEmi: string;
-            dhSaiEnt: string;
-            modal: string;
-            tpServ: string;
-            nCFOP: string;
-            tpEmis: string;
-            xMunFim: string;
-            UFFim: string;
-            UFIni: string;
-            xMunIni: string;
-        };
-        total: {
-            ICMSTot: { vNF: string };
-        };
-    
-        emitente: {
-            xNome: string;
-            xFant: string;
-            CNPJ: string;
-            CNAE: string;
-            IM: string;
-            enderEmit: {
-            fone: string;
-            UF: string;
-            nro: string;
-            CEP: string;
-            xBairro: string;
-            xPais: string;
-            xMun: string;
-            xLgr: string;
-            };
-            IE: string;
-        };
-        remetente: {
-            xNome: string;
-            xFant: string;
-            CNPJ: string;
-            CNAE: string;
-            IM: string;
-            enderReme: {
-            fone: string;
-            UF: string;
-            nro: string;
-            CEP: string;
-            xBairro: string;
-            xPais: string;
-            xMun: string;
-            xLgr: string;
-            };
-            IE: string;
-        };
-        destinatario: {
-            xNome: string;
-            xFant: string;
-            CNPJ: string;
-            CNAE: string;
-            IM: string;
-            enderDest: {
-            fone: string;
-            UF: string;
-            nro: string;
-            CEP: string;
-            xBairro: string;
-            xPais: string;
-            xMun: string;
-            xLgr: string;
-            };
-            IE: string;
-        };
-        versao: string;
-        impostos: {
-            ICMS: {
-            ICMS00: { CST: string; vBC: string; vICMS: string; pICMS: string};
-            ICMS20: { CST: string; vBC: string; pRedBC: string; vICMS: string; pICMS: string };
-            ICMS45: { CST: string;};
-            ICMS60: { CST: string; vBCSTRet: string; vICMSSTRet: string; pICMSSTRet: string };
-            ICMS90: { CST: string; pRedBC: string; vBC: string; pICMS: string; vICMS: string };
-            ICMSOutraUF: {
-                CST: string;
-                pRedBCOutraUF: string;
-                vBCOutraUF: string;
-                pICMSOutraUF: string;
-                vICMSOutraUF: string;
-            };
-            ICMSSN: {
-                CST: string;
-                indSN: string;
-                vTotTrib: string;
-                infAdFisco: string;
-            };
-            };
-        };
-        valores_servicos: {
-            vTPrest: string;
-            vRec: string
-        };
-        sefaz: {
-            status: string;
-            descricao: string;
-        };
-        informacoes_normal_substituto: {
-        infCarga: {
-            vCarga: string;
-            proPred: string;
-            xOutCat: string;
-            infQ: [
-                {
-                    tpMed: string;
-                    cUnid: string;
-                    qCarga: string;
-                }
-                ]
-            | {
-                tpMed: string;
-                cUnid: string;
-                qCarga: string;
-                };
-        };
-        infDoc: {
-            infNFe: [
-                {
-                    chave: string;
-                    nDoc: string;
-                    serie: string;
-                    dEmi: string;
-                    vNF: string;
-                }
-                ]
-            | {
-                chave: string;
-                nDoc: string;
-                serie: string;
-                dEmi: string;
-                vNF: string;
-                };
-        };
-        };
-        
+            vNF: string;
+          };
+    };
+  };
 }
 
 export interface AbaProps {
-    data: CteXmlProps
+  data: CteXmlProps;
+}
+
+export interface ICteGridData {
+  chave_nota: string;
+  nota: string;
+  serie: string;
+  emit_cnpj: string;
+  emit_nome: string;
+  dt_hr_emi: Date;
+  dhEmi: Date;
+  dEmi: Date;
 }
