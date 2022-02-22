cfc3a294723618c645e3f4be33c874cbb204a71d
TauDuque <hectauduque@gmail.com>
2022-02-22 10:32:24 -0300

    subj : certificado upload new url path

--- src/services/empresas/certificado-api/sendCertificate.ts
+++ src/services/empresas/certificado-api/sendCertificate.ts
@@ -1,22 +1,27 @@
-import api from "@services/api";
+import api from '@services/api';
 
 interface CertificateProps {
-    company_id: number;
-    certificado: string;
-    senha: string;
-    data_inicio: Date;
-    data_vencimento: Date
+  company_id: number;
+  certificado: string;
+  senha: string;
+  data_inicio: Date;
+  data_vencimento: Date;
 }
 
-export default async function sendCertificate(certificate: any, data: CertificateProps) {
-    const formData = new FormData()
-  
-    formData.append('certificado', certificate)
-    formData.append('senha', data.senha)
-    formData.append('data_inicio', data.data_inicio.toString())
-    formData.append('data_vencimento', data.data_vencimento.toString())
+export default async function sendCertificate(
+  certificate: any,
+  data: CertificateProps,
+) {
+  const formData = new FormData();
 
+  formData.append('certificado', certificate);
+  formData.append('senha', data.senha);
+  formData.append('data_inicio', data.data_inicio.toString());
+  formData.append('data_vencimento', data.data_vencimento.toString());
 
-    const response = api.post(`/empresas/certificado/criar/${data.company_id}`, formData);
-    return response
+  const response = api.post(
+    `/empresas/certificado/${data.company_id}`,
+    formData,
+  );
+  return response;
 }
