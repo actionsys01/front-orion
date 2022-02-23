88be10d2edcd60eeeccc93f15fd424f00966200a
TauDuque <hectauduque@gmail.com>
2022-02-23 11:26:22 -0300

    subj: new request params

--- src/services/empresas/cnpj-api/updateCnpj.ts
+++ src/services/empresas/cnpj-api/updateCnpj.ts
@@ -1,16 +1,19 @@
-import api from "@services/api";
-
+import api from '@services/api';
 
 interface UpdateCnpjToCompany {
-    cnpj : string;
-    nome : string;
-    nsu  : string;
-    uf : string;
-    status_sefaz : number;
+  cnpj: string;
+  nome: string;
+  nfe: boolean;
+  cte: boolean;
+  uf: string;
+  status_sefaz: number;
 }
 
-export default async function updateCnpj(id : number, data: UpdateCnpjToCompany) {
-    const response = await api.put(`/empresas/cnpj/${id}`, data)
-    
-    return response
+export default async function updateCnpj(
+  id: number,
+  data: UpdateCnpjToCompany,
+) {
+  const response = await api.put(`/empresas/cnpj/${id}`, data);
+
+  return response;
 }
