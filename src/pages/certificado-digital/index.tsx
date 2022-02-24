94993a337e6e9aecfea8699c0823e46094234766
TauDuque <hectauduque@gmail.com>
2022-02-24 18:10:15 -0300

    subj : some eslint fixe

--- src/pages/certificado-digital/index.tsx
+++ src/pages/certificado-digital/index.tsx
@@ -15,198 +15,184 @@ import DeletarModal from './delete-modal';
 import { certificateState } from '@utils/initial-states';
 
 export default function CertificadoDigital() {
-    // modais
-    const [visibleModal, setVisibleModal] = useState(false);
-    const [secondModal, setSecondModal] = useState(false);
-    const [deleteModal, setDeleteModal] = useState(false);
-    //
-    const [pageData, setPageData] = useState({ ...certificateState });
-    const [upload, setUpload] = useState(false);
-    const router = useRouter();
-    const [session] = useSession();
-    const company_id = Number(session?.usuario.empresa.id);
-    const { isCertificated, setIsCertificated } = useCompanyContext();
-    const { certificatePermissions } = useSecurityContext();
-    const [, setToast] = useToasts();
+  // modais
+  const [visibleModal, setVisibleModal] = useState(false);
+  const [secondModal, setSecondModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+  //
+  const [pageData, setPageData] = useState({ ...certificateState });
+  const [upload, setUpload] = useState(false);
+  const router = useRouter();
+  const [session] = useSession();
+  const company_id = Number(session?.usuario.empresa.id);
+  const { isCertificated, setIsCertificated } = useCompanyContext();
+  const { certificatePermissions } = useSecurityContext();
+  const [, setToast] = useToasts();
 
-    // console.log(`router.query`, router.query)
-    // console.log(`isCertificated`, isCertificated)
+  // console.log(`router.query`, router.query)
+  // console.log(`isCertificated`, isCertificated)
 
-    function uploadModalHandler() {
-        if (router.query.isCertificated === 'false' && !isCertificated) {
-            setVisibleModal(true);
-        } else setVisibleModal(false);
+  function uploadModalHandler() {
+    if (router.query.isCertificated === 'false' && !isCertificated) {
+      setVisibleModal(true);
+    } else setVisibleModal(false);
+  }
+  useEffect(() => {
+    uploadModalHandler();
+  }, [isCertificated]);
+
+  useEffect(() => {
+    if (!isCertificated && upload) {
+      setSecondModal(true);
     }
-    useEffect(() => {
-        uploadModalHandler();
-    }, [isCertificated]);
+  }, [upload]);
 
-    useEffect(() => {
-        if (!isCertificated && upload) {
-            setSecondModal(true);
-        }
-    }, [upload]);
+  const modalHandler = useCallback(() => {
+    setVisibleModal(!visibleModal);
+  }, [visibleModal]);
 
-    const modalHandler = useCallback(() => {
-        setVisibleModal(!visibleModal);
-    }, [visibleModal]);
+  const secondModalHandler = useCallback(() => {
+    setSecondModal(!secondModal);
+  }, [secondModal]);
 
-    const secondModalHandler = useCallback(() => {
-        setSecondModal(!secondModal);
-    }, [secondModal]);
+  const deleteModalHandler = useCallback(() => {
+    setDeleteModal(!deleteModal);
+  }, [deleteModal]);
 
-    const deleteModalHandler = useCallback(() => {
-        setDeleteModal(!deleteModal);
-    }, [deleteModal]);
-
-    async function getCerficateData() {
-        try {
-            const response = await companyRequest.getCertificate(company_id);
-            const data = response.data;
-            setPageData({
-                ...pageData,
-                initialDate: data.data_inicio,
-                expiringDate: data.data_vencimento,
-            });
-            return data;
-        } catch (error) {
-            // console.log(error)
-            // setToast({
-            //         text: "Houve um problema, por favor tente novamente",
-            //         type: "warning"
-            //     })
-        }
+  async function getCerficateData() {
+    try {
+      const response = await companyRequest.getCertificate(company_id);
+      const data = response.data;
+      setPageData({
+        ...pageData,
+        initialDate: data.data_inicio,
+        expiringDate: data.data_vencimento,
+      });
+      return data;
+    } catch (error) {
+      // console.log(error)
+      // setToast({
+      //         text: "Houve um problema, por favor tente novamente",
+      //         type: "warning"
+      //     })
     }
+  }
 
-    async function deleteCertificate() {
-        try {
-            await companyRequest.deleteCertificate(
-                Number(session.usuario.empresa.id),
-            );
-            setToast({
-                text: 'Certificado excluído com sucesso',
-                type: 'success',
-            });
-            setIsCertificated(false);
-            getCerficateData();
-        } catch (error) {
-            console.log(error);
-            setToast({
-                text: 'Houve um problema, por favor tente novamente',
-                type: 'warning',
-            });
-        }
-        deleteModalHandler();
+  async function deleteCertificate() {
+    try {
+      await companyRequest.deleteCertificate(
+        Number(session.usuario.empresa.id),
+      );
+      setToast({
+        text: 'Certificado excluído com sucesso',
+        type: 'success',
+      });
+      setIsCertificated(false);
+      getCerficateData();
+    } catch (error) {
+      console.log(error);
+      setToast({
+        text: 'Houve um problema, por favor tente novamente',
+        type: 'warning',
+      });
     }
+    deleteModalHandler();
+  }
 
-    useEffect(() => {
-        getCerficateData();
-    }, []);
+  useEffect(() => {
+    getCerficateData();
+  }, []);
 
-    return (
-        <>
-            <Head>
-                <title>Orion | Certificado Digital</title>
-            </Head>
-            <h2>Certificado Digital</h2>
-            <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    height: '85%',
-                }}
-            >
-                <Section>
-                    <div>
-                        <div>
-                            <InlineInputs>
-                                <span>Validade</span>
-                                <div>
-                                    <span>De:</span>
-                                    <input
-                                        type="text"
-                                        readOnly
-                                        value={
-                                            isCertificated
-                                                ? format(
-                                                      new Date(
-                                                          pageData.initialDate,
-                                                      ),
-                                                      'dd/MM/yyyy',
-                                                  )
-                                                : ''
-                                        }
-                                    />
-                                    <span>Até:</span>
-                                    <input
-                                        type="text"
-                                        readOnly
-                                        value={
-                                            isCertificated
-                                                ? format(
-                                                      new Date(
-                                                          pageData.expiringDate,
-                                                      ),
-                                                      'dd/MM/yyyy',
-                                                  )
-                                                : ''
-                                        }
-                                    />
-                                </div>
-                            </InlineInputs>
-                        </div>
-                        <RightInput>
-                            <span>Status:</span>
-                            <div>
-                                <div
-                                    className={isCertificated ? 'confirm' : ''}
-                                ></div>
-                            </div>
-                            {isCertificated && <span>Confirmado</span>}
-                        </RightInput>
-                    </div>
-                </Section>
-                <BtnRow>
-                    <button
-                        disabled={!certificatePermissions.EXCLUIR}
-                        onClick={() => setDeleteModal(true)}
-                    >
-                        <span>
-                            <Trash2 />
-                        </span>
-                        deletar
-                    </button>
-                    <button
-                        disabled={!certificatePermissions.ADICIONAR}
-                        onClick={() => setVisibleModal(true)}
-                    >
-                        <span>
-                            <Plus />
-                        </span>
-                        adicionar
-                    </button>
-                </BtnRow>
+  return (
+    <>
+      <Head>
+        <title>Orion | Certificado Digital</title>
+      </Head>
+      <h2>Certificado Digital</h2>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '85%',
+        }}
+      >
+        <Section>
+          <div>
+            <div>
+              <InlineInputs>
+                <span>Validade</span>
+                <div>
+                  <span>De:</span>
+                  <input
+                    type="text"
+                    readOnly
+                    value={
+                      isCertificated
+                        ? format(new Date(pageData.initialDate), 'dd/MM/yyyy')
+                        : ''
+                    }
+                  />
+                  <span>Até:</span>
+                  <input
+                    type="text"
+                    readOnly
+                    value={
+                      isCertificated
+                        ? format(new Date(pageData.expiringDate), 'dd/MM/yyyy')
+                        : ''
+                    }
+                  />
+                </div>
+              </InlineInputs>
             </div>
-            {visibleModal && (
-                <Modal
-                    modalHandler={modalHandler}
-                    setUpload={setUpload}
-                    pageData={pageData}
-                    setPageData={setPageData}
-                />
-            )}
-            {secondModal && (
-                <CnpjsModal secondModalHandler={secondModalHandler} />
-            )}
-            {deleteModal && (
-                <DeletarModal
-                    deleteModalHandler={deleteModalHandler}
-                    deleteCertificate={deleteCertificate}
-                />
-            )}
-        </>
-    );
+            <RightInput>
+              <span>Status:</span>
+              <div>
+                <div className={isCertificated ? 'confirm' : ''}></div>
+              </div>
+              {isCertificated && <span>Confirmado</span>}
+            </RightInput>
+          </div>
+        </Section>
+        <BtnRow>
+          <button
+            disabled={!certificatePermissions.EXCLUIR}
+            onClick={() => setDeleteModal(true)}
+          >
+            <span>
+              <Trash2 />
+            </span>
+            deletar
+          </button>
+          <button
+            disabled={!certificatePermissions.ADICIONAR}
+            onClick={() => setVisibleModal(true)}
+          >
+            <span>
+              <Plus />
+            </span>
+            adicionar
+          </button>
+        </BtnRow>
+      </div>
+      {visibleModal && (
+        <Modal
+          modalHandler={modalHandler}
+          setUpload={setUpload}
+          pageData={pageData}
+          setPageData={setPageData}
+        />
+      )}
+      {secondModal && <CnpjsModal secondModalHandler={secondModalHandler} />}
+      {deleteModal && (
+        <DeletarModal
+          deleteModalHandler={deleteModalHandler}
+          deleteCertificate={deleteCertificate}
+        />
+      )}
+    </>
+  );
 }
 
 CertificadoDigital.auth = true;
