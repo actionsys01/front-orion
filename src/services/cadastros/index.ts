abfe2a606067328f2232ab92467095feb25fc4f0
TauDuque <hectauduque@gmail.com>
2022-03-07 15:17:28 -0300

    subj : new requests services/types - update/delete

--- src/services/cadastros/index.ts
+++ src/services/cadastros/index.ts
@@ -1,4 +1,6 @@
 import GetCategoryByCode from './cadastro-categorias/GetCategoryByCode';
 import GetConfigById from './cadastro-configs/GetConfigById';
+import DeleteConfig from './cadastro-configs/DeleteConfig';
+import UpdateConfig from './cadastro-configs/UpdateConfig';
 
-export { GetCategoryByCode, GetConfigById };
+export { GetCategoryByCode, GetConfigById, DeleteConfig, UpdateConfig };
