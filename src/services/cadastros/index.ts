db095056f49415a589cd28b81e6171fbd3b33d87
TauDuque <hectauduque@gmail.com>
2022-03-07 12:54:53 -0300

    subj : new requests to new feature

---
+++ src/services/cadastros/index.ts
@@ -0,0 +1,4 @@
+import GetCategoryByCode from './cadastro-categorias/GetCategoryByCode';
+import GetConfigById from './cadastro-configs/GetConfigById';
+
+export { GetCategoryByCode, GetConfigById };
