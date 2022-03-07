94271c92e5e14e1d09e455ae8268190b78afeaf7
TauDuque <hectauduque@gmail.com>
2022-03-07 16:51:54 -0300

    subj : new imports requests

--- src/services/cadastros/index.ts
+++ src/services/cadastros/index.ts
@@ -2,5 +2,12 @@ import GetCategoryByCode from './cadastro-categorias/GetCategoryByCode';
 import GetConfigById from './cadastro-configs/GetConfigById';
 import DeleteConfig from './cadastro-configs/DeleteConfig';
 import UpdateConfig from './cadastro-configs/UpdateConfig';
+import CreateDado from './cadastro-dados/CreateDado';
 
-export { GetCategoryByCode, GetConfigById, DeleteConfig, UpdateConfig };
+export {
+  GetCategoryByCode,
+  GetConfigById,
+  DeleteConfig,
+  UpdateConfig,
+  CreateDado,
+};
