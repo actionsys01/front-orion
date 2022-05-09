3146c0da585ad317a14afc202b6dbe582c628787
TauDuque <hectauduque@gmail.com>
2022-05-09 21:34:50 +0200

    subj : refresh btn

--- src/pages/nfse/style.ts
+++ src/pages/nfse/style.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const RowBtn = styled.div` 
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    margin-bottom: 10px;
-`
+export const RowBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: 10px;
+`;
