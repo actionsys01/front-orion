9b0029db0c0563a62192f45f693a30283486575a
TauDuque <hectauduque@gmail.com>
2022-05-10 14:43:06 +0200

    subj : botão de refresh em usuários

--- src/pages/usuarios/index.tsx
+++ src/pages/usuarios/index.tsx
@@ -11,144 +11,151 @@ import getUsersByCompanyId from '@services/usuarios/getUsersByCompanyId';
 import UserPopover from './Popover';
 import capitalize from '@utils/capitalize';
 import UserModal from './modal';
+import { RefreshCw } from '@geist-ui/react-icons';
+import { RefreshBtn } from '@styles/RefreshBtn';
 
 export interface IUsuario {
-    id: number;
-    nome: string;
-    email: string;
-    perfil: Perfil;
+  id: number;
+  nome: string;
+  email: string;
+  perfil: Perfil;
 }
 
 interface Perfil {
-    id: number;
-    nome: string;
-    descricao: string;
-    criadoEm: string;
-    atualizadaEm: string;
-    criadoPorIp: string;
-    atualizadoPorIp: string;
+  id: number;
+  nome: string;
+  descricao: string;
+  criadoEm: string;
+  atualizadaEm: string;
+  criadoPorIp: string;
+  atualizadoPorIp: string;
 }
 
 export default function Usuarios() {
-    const { userPermissions } = useSecurityContext();
-    const router = useRouter();
-    const [usuarios, setUsuarios] = useState<IUsuario[]>([]);
-    const [page, setPage] = useState(1);
-    const [quantityPage, setQuantityPage] = useState(1);
-    const [userModalVisible, setUserModalVisible] = useState(false);
+  const { userPermissions } = useSecurityContext();
+  const router = useRouter();
+  const [usuarios, setUsuarios] = useState<IUsuario[]>([]);
+  const [page, setPage] = useState(1);
+  const [quantityPage, setQuantityPage] = useState(1);
+  const [userModalVisible, setUserModalVisible] = useState(false);
 
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value);
-    };
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
-    const getUsersAndTotalPage = useCallback(async () => {
-        const responseNfes: any = await getUsersByCompanyId(page);
-        const { data } = responseNfes;
+  const getUsersAndTotalPage = useCallback(async () => {
+    const responseNfes: any = await getUsersByCompanyId(page);
+    const { data } = responseNfes;
 
-        setUsuarios(data.usuarios);
+    setUsuarios(data.usuarios);
 
-        setQuantityPage(Math.ceil(data.total / 8));
-    }, [page]);
+    setQuantityPage(Math.ceil(data.total / 8));
+  }, [page]);
 
-    useEffect(() => {
-        getUsersAndTotalPage();
-    }, [page]);
+  useEffect(() => {
+    getUsersAndTotalPage();
+  }, [page]);
 
-    function optionUserPopover(data: any) {
-        return (
-            <UserPopover
-                data={data}
-                usuarios={usuarios}
-                setUsuarios={setUsuarios}
-                setUserModalVisible={setUserModalVisible}
-            />
-        );
-    }
-
-    const UsersByCompanyData = useMemo(() => {
-        const allData: any = [];
-        if (usuarios) {
-            usuarios.forEach(item => {
-                allData.push({
-                    ...item,
-                    perfil_nome: item.perfil.nome,
-                    emailFormatted: item.email.toLowerCase(),
-                    nome: capitalize(item.nome),
-                    option: optionUserPopover(item),
-                });
-            });
-        }
-
-        return allData.sort(comparations);
-    }, [usuarios]);
-
-    function comparations(a: any, b: any) {
-        if (a.nome < b.nome) {
-            return -1;
-        }
-        if (a.nome > b.nome) {
-            return 1;
-        }
-        return 0;
-    }
-
+  function optionUserPopover(data: any) {
     return (
-        <>
-            <Head>
-                <title>Orion | Usuários</title>
-            </Head>
-            <Text h2>Usuários</Text>
-            <Row justify="end" align="middle">
-                <Button
-                    type="success-light"
-                    size="small"
-                    icon={<Plus />}
-                    disabled={!userPermissions.ADICIONAR}
-                    onClick={() => router.push('/cadastrar-usuario')}
-                >
-                    Adicionar
-                </Button>
-            </Row>
-            <Spacer y={1} />
-            <GridStyle>
-                <table>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Nome</th>
-                            <th>E-mail</th>
-                            <th>Perfil</th>
-                        </tr>
-                    </thead>
-                    {userPermissions.ADICIONAR && (
-                        <tbody>
-                            {UsersByCompanyData.map((item: any, i: any) => (
-                                <tr key={i}>
-                                    <td>{item.option}</td>
-                                    <td style={{ textTransform: 'capitalize' }}>
-                                        {item.nome}
-                                    </td>
-                                    <td>{item.email}</td>
-                                    <td>{item.perfil_nome}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    )}
-                </table>
-            </GridStyle>
-            <Pages>
-                <Pagination
-                    style={{ margin: '0 auto' }}
-                    onChange={handleChange}
-                    count={quantityPage}
-                    shape="rounded"
-                />
-            </Pages>
-            {userModalVisible && (
-                <UserModal setUserModalVisible={setUserModalVisible} />
-            )}
-        </>
+      <UserPopover
+        data={data}
+        usuarios={usuarios}
+        setUsuarios={setUsuarios}
+        setUserModalVisible={setUserModalVisible}
+      />
     );
+  }
+
+  const UsersByCompanyData = useMemo(() => {
+    const allData: any = [];
+    if (usuarios) {
+      usuarios.forEach(item => {
+        allData.push({
+          ...item,
+          perfil_nome: item.perfil.nome,
+          emailFormatted: item.email.toLowerCase(),
+          nome: capitalize(item.nome),
+          option: optionUserPopover(item),
+        });
+      });
+    }
+
+    return allData.sort(comparations);
+  }, [usuarios]);
+
+  function comparations(a: any, b: any) {
+    if (a.nome < b.nome) {
+      return -1;
+    }
+    if (a.nome > b.nome) {
+      return 1;
+    }
+    return 0;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Orion | Usuários</title>
+      </Head>
+      <Text h2>Usuários</Text>
+      <Row justify="end" align="middle">
+        <RefreshBtn>
+          <RefreshCw
+            onClick={() => {
+              getUsersAndTotalPage();
+            }}
+          />
+        </RefreshBtn>
+        <Button
+          type="success-light"
+          size="small"
+          icon={<Plus />}
+          disabled={!userPermissions.ADICIONAR}
+          onClick={() => router.push('/cadastrar-usuario')}
+        >
+          Adicionar
+        </Button>
+      </Row>
+      <Spacer y={1} />
+      <GridStyle>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Perfil</th>
+            </tr>
+          </thead>
+          {userPermissions.ADICIONAR && (
+            <tbody>
+              {UsersByCompanyData.map((item: any, i: any) => (
+                <tr key={i}>
+                  <td>{item.option}</td>
+                  <td style={{ textTransform: 'capitalize' }}>{item.nome}</td>
+                  <td>{item.email}</td>
+                  <td>{item.perfil_nome}</td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </GridStyle>
+      <Pages>
+        <Pagination
+          style={{ margin: '0 auto' }}
+          onChange={handleChange}
+          count={quantityPage}
+          shape="rounded"
+        />
+      </Pages>
+      {userModalVisible && (
+        <UserModal setUserModalVisible={setUserModalVisible} />
+      )}
+    </>
+  );
 }
 
 Usuarios.auth = true;
