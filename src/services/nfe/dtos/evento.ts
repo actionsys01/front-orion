ee0a26800ea13cf275c526aa69ba1833ae8535b6
TauDuque <hectauduque@gmail.com>
2022-05-11 14:07:45 +0200

    subj : nova tipagem de service de eventos

---
+++ src/services/nfe/dtos/evento.ts
@@ -0,0 +1,9 @@
+export default interface IEvento {
+  motivo: string;
+  tipo_evento: string;
+  cod_estado: string;
+  chave_nota: string;
+  empresa_id: number;
+  dest_cnpj: string;
+  ambiente: string;
+}
