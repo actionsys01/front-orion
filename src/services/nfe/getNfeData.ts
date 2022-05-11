283b3200169fbff3f2b5ece37f57fe2eac1f5827
TauDuque <hectauduque@gmail.com>
2022-05-11 14:07:21 +0200

    subj : novo service e ajustes eslint

--- src/services/nfe/getNfeData.ts
+++ src/services/nfe/getNfeData.ts
@@ -1,10 +1,10 @@
-import api from "@services/api";
+import api from '@services/api';
 
 export default async function getNfeData(chave: string, company_id: number) {
-    const response = await api.get(`/nfe/xml/${chave}`, {
-        params: {
-            company_id
-        }
-    });
-    return response
+  const response = await api.get(`/nfe/xml/${chave}`, {
+    params: {
+      company_id,
+    },
+  });
+  return response;
 }
